06dd30a16ab43f62da8643c8f6fd5b9cf696cb6f
David Fellows <dfellows12@gmail.com>
2019-04-20 02:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians, instrument) {
   var music = [];
-  for (var i = 0; musicians.length; i++) {
+  for (var i = 0; l = musicians.length; i < l; i++) {
     music.push(`${musicians[i]} plays ${instrument[i]}`);
   }
   return music;
